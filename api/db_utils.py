b33de3381381294604d16b5f8e4bbe6205308f7a
Munisha B <78922862+munishaboora@users.noreply.github.com>
2022-08-04 07:29:26 +0100

    Update db_utils.py

---
+++ api/db_utils.py
@@ -0,0 +1,10 @@
+#
+
+def database_connection():
+    import mysql.connector
+    quiz_db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="quiz_game")
+    return quiz_db
